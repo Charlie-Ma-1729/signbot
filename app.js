e8125a51e5cd20f2268e170a34da6f86a4a05d9a
TGoC_virus <61151352+TGoC-virus@users.noreply.github.com>
2021-11-02 17:14:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/', async function (req, res) {
     res.render('index');
 });
 const mongoDB = 'mongodb+srv://signbot:20201016@users.1zwb5.mongodb.net/SignInData?retryWrites=true&w=majority'
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("DB connect!")
     })
@@ -25,7 +25,7 @@ mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
         console.log(err)
     })
 
-    
+
 const Schema = mongoose.Schema
 // 先新增 Author 的 Schema ，其中可設定姓名、生卒年等，可對其 value 的規格做限制。
 const UserSchema = new Schema(
@@ -43,6 +43,9 @@ const UserSchema = new Schema(
         tf: {
             type: Number,
             default: 1
+        },
+        goal: {
+            type: Number
         }
     })
 
@@ -53,7 +56,7 @@ const UserData = mongoose.model('users', UserSchema)
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     const job = schedule.scheduleJob('0 16 * * *', async function () {
-        await UserData.updateMany({}, {$set: {tf: 0}})
+        await UserData.updateMany({}, { $set: { tf: 0 } })
     });
 });
 
@@ -62,7 +65,7 @@ client.on("message", async msg => {
         await msg.react("📝")
         let username = msg.author.id
         let serverid = msg.guild.id
-        if (!await UserData.findOne({server: serverid, userid: username})) {
+        if (!await UserData.findOne({ server: serverid, userid: username })) {
             const newUser = new UserData({
                 userid: username,
                 server: serverid
@@ -70,19 +73,38 @@ client.on("message", async msg => {
             await newUser.save()
             msg.channel.send('哇喔!是**' + msg.author.username + '**耶~ 恭喜你完成了第一次簽到!希望之後能很常見到你~');
         } else {
-            let data = await UserData.findOne({server: serverid, userid: username});
+            let data = await UserData.findOne({ server: serverid, userid: username });
             if (data.tf === 1) {
                 msg.channel.send('**' + msg.author.username + '**，你今天已經簽到過了喔!明天再來吧~');
             } else {
-                await UserData.updateOne({server: serverid, userid: username}, {$inc: {count: 1}})
-                await UserData.updateOne({server: serverid, userid: username}, {$inc: {tf: 1}})
-                let data = await UserData.findOne({server: serverid, userid: username});
-                msg.channel.send('哇喔!是**' + msg.author.username + '**耶~ 今天也很高興見到你');
-                msg.channel.send('你已經累計簽到了**' + data.count + '**次囉!');
+                await UserData.updateOne({ server: serverid, userid: username }, { $inc: { count: 1 } })
+                await UserData.updateOne({ server: serverid, userid: username }, { $inc: { tf: 1 } })
+                let data = await UserData.findOne({ server: serverid, userid: username });
+                if (data.goal === data.count) {
+                    msg.channel.send('🎉🎉🎉恭喜**' + msg.author.username + '**~');
+                    msg.channel.send('你達成了你的簽到目標**' + data.count + '**次囉!');
+                } else {
+                    msg.channel.send('哈囉**' + msg.author.username + '**~ 今天也很高興見到你');
+                    msg.channel.send('你已經累計簽到了**' + data.count + '**次囉!');
+                }
             }
         }
 
     }
+    if (msg.content.substring(0, 9) == '!setgoal ') {
+        await msg.react("🎯")
+        let username = msg.author.id
+        let serverid = msg.guild.id
+        if (!await UserData.findOne({ server: serverid, userid: username })) {
+            msg.channel.send('哈囉**' + msg.author.username + '**~ 請先完成第一次簽到再設定目標喔!');
+        } else {
+            await UserData.updateOne({ server: serverid, userid: username }, { $set: { goal: msg.content.substring(10, 2147483647) } })
+            let data = await UserData.findOne({ server: serverid, userid: username });
+            msg.channel.send('**' + msg.author.username + '**設下了他的簽到目標為' + data.goal + '次!');
+        }
+    }
+
+}
 });
 app.listen(port_number, () => console.log('Server up and running'));
 client.login(token);
